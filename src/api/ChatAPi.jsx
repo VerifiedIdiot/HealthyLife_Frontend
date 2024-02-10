6b661e06f21a24dd992903a1454e75310ec4b60c
incheons <sonincheon94@naver.com>
2024-02-10 16:36:51 +0900

    2024/02/10 손인천 채팅 최신 메세지 읽음 안읽음

--- src/api/ChatAPi.jsx
+++ src/api/ChatAPi.jsx
@@ -25,32 +25,72 @@ const ChatApi = {
     console.log("채팅방생성" + memberId + "_" + senderId);
     return await AxiosInstance.post(Common.WEELV_DOMAIN + "/chat/new", chat);
   },
-// 메시지 저장
-saveMessage: async (message) => {
-  try {
-    const chat = {
-      message: message.message, // 수정: message.content가 아니라 message.message여야 함
-      roomId: message.roomId,
-      sender: message.sender, // 수정: message.sneder가 아니라 message.sender여야 함
-      type: "TALK",
-    };
-    console.log("저장하기", message); // 수정: 문자열과 객체를 함께 출력하려면 쉼표로 구분
-    const response = await AxiosInstance.post(
-      Common.WEELV_DOMAIN + "/chat/message",
-      chat
-    );
-    console.log("메시지 저장 성공:", response.data);
-  } catch (error) {
-    console.error("메시지 저장 중 에러 발생:", error);
-    // 에러 처리 로직 추가
-  }
-},
+  // 읽지 않은 메시지 수 가져오기
+  getUnreadMessageCount: async (roomId, memberId) => {
+    try {
+      const response = await AxiosInstance.get(
+        Common.WEELV_DOMAIN + `/chat/unreadMessages/${roomId}/${memberId}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("읽지 않은 메시지 수 조회 중 에러 발생:", error);
+      // 에러 처리 로직 추가
+      return 0; // 예외가 발생하면 -1을 반환하거나 다른 적절한 값으로 처리할 수 있습니다.
+    }
+  },
+  // 채팅방의 최신 메시지 가져오기
+  getLatestMessage: async (roomId) => {
+    try {
+      const response = await AxiosInstance.get(
+        Common.WEELV_DOMAIN + `/chat/latestMessage/${roomId}`
+      );
+      return response.data.message;
+    } catch (error) {
+      console.error("최신 메시지 조회 중 에러 발생:", error);
+      // 에러 처리 로직 추가
+      return null; // 예외가 발생하면 null을 반환하거나 다른 적절한 값으로 처리할 수 있습니다.
+    }
+  },
+  // 메시지 저장
+  saveMessage: async (message) => {
+    try {
+      const chat = {
+        message: message.message, // 수정: message.content가 아니라 message.message여야 함
+        roomId: message.roomId,
+        sender: message.sender, // 수정: message.sneder가 아니라 message.sender여야 함
+        type: "TALK",
+      };
+      console.log("저장하기", message); // 수정: 문자열과 객체를 함께 출력하려면 쉼표로 구분
+      const response = await AxiosInstance.post(
+        Common.WEELV_DOMAIN + "/chat/message",
+        chat
+      );
+      console.log("메시지 저장 성공:", response.data);
+    } catch (error) {
+      console.error("메시지 저장 중 에러 발생:", error);
+      // 에러 처리 로직 추가
+    }
+  },
   //채팅방 메세지 가져오기
   takenMessage: async (roomId) => {
     console.log("메세지 가져오기" + roomId);
     return await AxiosInstance.post(
       Common.WEELV_DOMAIN + `/chat/messages/${roomId}`
     );
+  },
+
+  updateMessageStatus: async (roomId, userId) => {
+    try {
+      // 서버에 메시지 읽음 상태 업데이트 요청 보내기
+      await AxiosInstance.post(
+        Common.WEELV_DOMAIN +
+          `/chat/updateMessageStatus?memberId=${userId}&roomId=${roomId}&status="읽음"`
+      );
+      console.log("읽음 상태 업데이트 성공");
+    } catch (error) {
+      console.error("메시지 읽음 상태 업데이트 실패:", error);
+      // 에러 처리 로직 추가
+    }
   },
   ////////////////////////////////////친구차단 관리 ////////////////////////////////////////////////////
   //친구목록출력

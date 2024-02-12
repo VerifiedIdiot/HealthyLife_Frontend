fe1fba8a3cd458ce919b9a39e1dc67d9c386050f
lastofyears21 <lastofyears21@gmail.com>
2024-02-12 13:24:58 +0900

    24/02/12 정벼리 context + reducer 기본 골자 캘린더 영역에 추가

--- src/contexts/CalendarContext.jsx
+++ src/contexts/CalendarContext.jsx
@@ -1,22 +1,36 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useLayoutEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export const CalendarContext = createContext(null);
+export const CalendarContext = createContext();
 
-export const CalendarProvider = (props) => {
-  const [addedMeals, setAddedMeals] = useState({});
-  const [isTrue, setIsTrue] = useState();
+const initialState = {
+ 
+};
+
+const actions = {
+
+
+}
+
+const calendarReducer = (state, action) => {
+  switch (action.type) {
+    case "" :
+      if ("" === "".payload) {
+        return state;
+      }
+  }
+}
+
+
+
+export const CalendarProvider = ({children}) => {
+  const [state, dispatch] = useReducer(calendarReducer, initialState);
+  const navigate = useNavigate();
+  const location = useLocation();  
 
   return (
-    <CalendarContext.Provider
-      value={{
-        addedMeals,
-        setAddedMeals,
-        isTrue,
-        setIsTrue,
-        
-      }}
-    >
-      {props.children}
+    <CalendarContext.Provider value={{state, actions}}>
+    {children}
     </CalendarContext.Provider>
   );
 };

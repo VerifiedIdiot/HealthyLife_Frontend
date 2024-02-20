341b16064b6b8fb6b2ad51af4244232987a42891
lastofyears21 <lastofyears21@gmail.com>
2024-02-20 15:02:43 +0900

    24/02/20 정벼리 중간점검

--- src/components/medicinePage/ReactTable.jsx
+++ src/components/medicinePage/ReactTable.jsx
@@ -5,7 +5,7 @@ import { useTable } from "react-table";
 import { useMobileView, useTabletView } from "../../hooks/useMobileView";
 
 const TableArea = styled.table`
-  max-width: 100%;
+  width: 100%;
   border-collapse: collapse;
   background-color: #fff;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
@@ -61,7 +61,6 @@ const TableHeaderCell = styled.th`
   font-weight: bold;
 
   @media (max-width: 768px) {
-    
   }
 
   @media (max-width: 500px) {
@@ -69,65 +68,71 @@ const TableHeaderCell = styled.th`
 `;
 const TableBody = styled.tbody`
   vertical-align: middle;
+  
 `;
 const TableDataCell = styled.td`
   height: 8vh;
   vertical-align: middle;
+  width: ${(props) => `${props.width}`};
   padding: 5px;
   border-top: 1px solid #ddd;
   text-align: left;
-  cursor: pointer;
+
+  white-space: normal;
 
   @media (max-width: 768px) {
+    cursor: pointer;
     max-width: 32vw;
     overflow: hidden; // 내용이 넘칠 경우 숨김
-  text-overflow: ellipsis; // 넘친 텍스트는 ...으로 표시
-  white-space: nowrap; // 텍스트를 한 줄로 표시
+    text-overflow: ellipsis; // 넘친 텍스트는 ...으로 표시
+    white-space: nowrap; // 텍스트를 한 줄로 표시
 
-    
-  &:hover {
-    overflow: visible; // 호버 시 전체 텍스트를 보이도록 설정 (선택적)
-    white-space: normal; // 호버 시 텍스트 줄바꿈 허용 (선택적)
-    z-index: 1; // 다른 셀 위에 텍스트를 보이도록 z-index 설정 (선택적)
-  }
+    &:hover {
+      overflow: visible; // 호버 시 전체 텍스트를 보이도록 설정 (선택적)
+      white-space: normal; // 호버 시 텍스트 줄바꿈 허용 (선택적)
+      z-index: 1; // 다른 셀 위에 텍스트를 보이도록 z-index 설정 (선택적)
+    }
   }
 
   @media (max-width: 500px) {
   }
 `;
 
-
-
 const ReactTable = () => {
   const { state, actions } = useSearch();
   const { searchResults, searchExecuted } = state;
   const isMobileView = useMobileView();
   const isTabletView = useTabletView();
-  const [expandedRow, setExpandedRow] = useState(null);
+  const [expandedCells, setExpandedCells] = useState({});
 
-  // 클릭 이벤트 핸들러: 선택된 행을 토글하고 다른 행이 열려있다면 닫습니다.
-  const handleRowClick = (rowId) => {
-    setExpandedRow(expandedRow === rowId ? null : rowId);
+  // 클릭 이벤트 핸들러: 모바일과 태블릿 환경에서만 작동
+  const handleCellClick = (rowId, columnId) => {
+    // 모바일 또는 태블릿 환경에서만 토글 동작 수행
+    if (isMobileView || isTabletView) {
+      const key = `${rowId}-${columnId}`;
+      setExpandedCells(prev => ({
+        ...prev,
+        [key]: !prev[key] // 현재 상태를 반전
+      }));
+    }
   };
-
   // 컬럼 너비를 환경에 따라 동적으로 설정
   const getColumnWidth = (key) => {
     // 모바일 또는 태블릿 환경일 때 사용할 너비(%)
     const widthPercentage = {
-      
       name: "20%",
-      
+
       functionalities: "33%",
       company: "33%",
     };
 
     // PC 환경일 때 사용할 너비(px)
     const widthPixels = {
-      reportNo: "200px",
-      name: "300px",
-      originType: "120px",
-      functionalities: "1000px",
-      company: "300px",
+      reportNo: "15%",
+      name: "20%",
+      originType: "10%",
+      functionalities: "50%",
+      company: "30%",
     };
 
     // 모바일 또는 태블릿 환경이면 % 단위, 그렇지 않으면 px 단위를 반환
@@ -153,7 +158,7 @@ const ReactTable = () => {
         Header: "제품 구분",
         accessor: "originType",
         show: !(isMobileView || isTabletView),
-        width: isMobileView ? 80 : isTabletView ? 100 : 120,
+
         width: getColumnWidth("originType"),
       },
 
@@ -161,7 +166,7 @@ const ReactTable = () => {
         Header: "기능성",
         accessor: "functionalities",
         Cell: ({ cell: { value } }) => value.join(", "), // 배열을 문자열로 변환
-        width: getColumnWidth("functionalities"),
+        // width: getColumnWidth("functionalities"),
       },
       {
         Header: "제조사명",
@@ -197,38 +202,25 @@ const ReactTable = () => {
         ))}
       </TableHeader>
       <TableBody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          const isExpanded = row.id === expandedRow; // 현재 행이 확장된 행인지 확인
-          return (
-            <TableBodyRow
-              {...row.getRowProps()}
-              onClick={() => handleRowClick(row.id)} // 클릭 이벤트 핸들러 연결
-              $shadow={isExpanded ? "0 8px 16px rgba(0,0,0,0.2)" : "0 2px 4px rgba(0,0,0,0.1)"}
-              // 확장된 행에 대한 추가 스타일링 (옵션)
-            >
-              {row.cells.map((cell) => {
-                // 컬럼의 show 속성을 확인하여 셀 렌더링
-                if (cell.column.show === false) return null;
-                return (
-                  <TableDataCell
-                    {...cell.getCellProps()}
-                    width={cell.column.width}
-                    style={{
-                      maxWidth: isExpanded ? "none" : "", // 확장된 행에 대한 스타일 조정
-                      whiteSpace: isExpanded ? "normal" : "nowrap",
-                      overflow: isExpanded ? "visible" : "hidden",
-                      textOverflow: isExpanded ? "" : "ellipsis",
-                    }}
-                  >
-                    {cell.render("Cell")}
-                  </TableDataCell>
-                );
-              })}
-            </TableBodyRow>
-          );
+  {rows.map((row) => {
+    prepareRow(row);
+    return (
+      <TableBodyRow {...row.getRowProps()}>
+        {row.cells.map((cell) => {
+          // 컬럼이 숨겨져 있지 않은 경우에만 셀 렌더링
+          if (cell.column.show !== false) {
+            return (
+              <TableDataCell {...cell.getCellProps()}>
+                {cell.render("Cell")}
+              </TableDataCell>
+            );
+          }
+          return null; // 컬럼이 숨겨진 경우 셀을 렌더링하지 않음
         })}
-      </TableBody>
+      </TableBodyRow>
+    );
+  })}
+</TableBody>
     </TableArea>
   );
 };
